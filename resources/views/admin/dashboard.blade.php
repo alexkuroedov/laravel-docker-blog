4d59c531e6b4e756a0b992235bb5b6612a4147bf
alex <alexdev12@yandex.ru>
2019-06-30 22:15:06 +0300

    add some html to admin page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,56 @@
 @extends('admin.layouts.app_admin')
 @section('content')
-    <h1>Admin test!</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p>
+                        <span class="label label-primary">Categories 0</span>
+                    </p>
+                </div>
+            </div>
+     
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p>
+                        <span class="label label-primary">News 0</span>
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p>
+                        <span class="label label-primary">Users 0</span>
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="jumbotron">
+                    <p>
+                        <span class="label label-primary">Today 0</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-block btn-default">Add category</a>
+                <a href="" class="list-group-item">
+                    <h4 class="list-group-item-heading">First category</h4>
+                    <p class="list-group-item-text">
+                        Total news
+                    </p>
+                </a>      
+            </div>
+            <div class="col-sm-6">
+                <a href="#" class="btn btn-block btn-default">Add news</a>
+                <a href="" class="list-group-item">
+                    <h4 class="list-group-item-heading">First news</h4>
+                    <p class="list-group-item-text">
+                        Total news
+                    </p>
+                </a>      
+            </div>
+        </div>
+    </div>
 @endsection
